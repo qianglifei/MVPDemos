c619c3ed255a118e1e69271c0517b451fcae8365
qianglifei <qianglifei99>
2020-09-02 18:07:13 +0800

    架构搭建

--- app/src/main/java/com/bksx/mobile/mvpdemo/epdemic_sutiation/EpidemicSituationFragment.java
+++ app/src/main/java/com/bksx/mobile/mvpdemo/epdemic_sutiation/EpidemicSituationFragment.java
@@ -16,7 +16,7 @@ import com.bksx.mobile.mvpdemo.R;
  * Use the {@link EpidemicSituationFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class EpidemicSituationFragment extends BaseFragment<> {
+public class EpidemicSituationFragment extends Fragment{
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
